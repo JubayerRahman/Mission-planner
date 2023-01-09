137457ea68da65c86c442f4e0c98b2935fedbcbf
JubayerRahman <jubayerr398@gmail.com>
2023-01-10 02:20:13 +0600

    Addea a localstorage version

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ const clearButton = document.getElementById('ClearButton')
 const lenth = []
 
 //main submit event
-document.addEventListener('DOMContentLoaded', getTodos);
 
 submitButton.addEventListener('click', ()=>{
     mNameError.innerText=""
@@ -94,10 +93,6 @@ submitButton.addEventListener('click', ()=>{
         todoDiv.appendChild(timeDiv);
         todoDiv.appendChild(buttonDiv);
         missionList.appendChild(todoDiv);
-
-        //adding items in the local storage
-        
-        saveLocalTodos(missionName.value , missionDescription.value, hour.value, minute.value);
         
 
 
@@ -147,113 +142,3 @@ submitButton.addEventListener('click', ()=>{
             
 }
 })
-
-function saveLocalTodos(todo, description, month, date) {
-    let toNames;
-    let todescription;
-    let tomonth;
-    let todate;
-    if (localStorage.getItem('toNames') === null) {
-        toNames = [];
-        todescription = [];
-        tomonth = [];
-        todate = [];
-    } 
-    else {
-        toNames = JSON.parse(localStorage.getItem('toNames'));
-        todescription = JSON.parse(localStorage.getItem('todescription'));
-        tomonth = JSON.parse(localStorage.getItem('tomonth'));
-        todate = JSON.parse(localStorage.getItem('todate'));
-    }
-    toNames.push(todo);
-    localStorage.setItem('toNames', JSON.stringify(toNames));
-    todescription.push(description);
-    localStorage.setItem('todescription', JSON.stringify(todescription));
-    tomonth.push(month);
-    localStorage.setItem('tomonth', JSON.stringify(tomonth));
-    todate.push(date);
-    localStorage.setItem('todate', JSON.stringify(todate));
-}
-
-function getTodos() {
-    let toNames;
-    let todescription;
-    let tomonth;
-    let todate;
-    if (localStorage.getItem('toNames') === null) {
-        toNames = [];
-        todescription = [];
-        tomonth = [];
-        todate = [];
-    }
-    else {
-        toNames = JSON.parse(localStorage.getItem('toNames'));
-        todescription = JSON.parse(localStorage.getItem('todescription'));
-        tomonth = JSON.parse(localStorage.getItem('tomonth'));
-        todate = JSON.parse(localStorage.getItem('todate'));
-    }
-    toNames.forEach(function(toNames){
-        clearButton.style.display = 'block'
-    document.getElementById('defaultDiv').style.display="none";
-        const todoDiv = document.createElement('div');
-        todoDiv.classList.add('missions')
-        const dataDiv = document.createElement('div');
-        dataDiv.classList.add('dataDiv')
-        const buttonDiv = document.createElement('div');
-        buttonDiv.classList.add('buttonDiv')
-        const todo = document.createElement('li');
-        todo.innerHTML ="Tittle: " + toNames;
-        missionList.appendChild(todo);
-        // Buttons 
-        const complete = document.createElement('button');
-        complete.innerHTML= '<i class="fa-solid fa-circle-check"></i>'
-        complete.classList.add('completeButton')
-        // const trashCan = document.createElement('button');
-        // trashCan.innerHTML= '<i class="fa-solid fa-trash-can"></i>'
-        // trashCan.classList.add('trashButton')
-        //adding items on subDiv
-        dataDiv.appendChild(todo);
-        buttonDiv.appendChild(complete);
-        // buttonDiv.appendChild(trashCan);
-        //adding items on main div
-        todoDiv.appendChild(dataDiv);
-        todoDiv.appendChild(buttonDiv);
-        missionList.appendChild(todoDiv);
-
-        //ADD SINGLE DELETE  BUtton option
-        missionList.addEventListener('click',(e)=>{
-            const item = (e.target)
-            const parenttodo= item.parentElement.parentElement;
-            if(item.classList[0]==='trashButton'){
-                parenttodo.classList.add('delanimation')
-                parenttodo.addEventListener('transitionend',()=>{
-                    parenttodo.remove();
-                const missions = document.querySelectorAll('.missions')
-                    console.log(missions.length)
-                    resultTitle.innerText= "Tasks: "+ (missions.length);
-                    if(missions.length===0){
-                        clearButton.style.display="none"
-                        defaultDiv.style.display="block"
-                    }
-                })
-            }
-            // Add single complete buttion Style
-            if(item.classList[0]==='completeButton'){
-                parenttodo.classList.add('completeMission')
-            }
-        })
-
-        const missions = document.querySelectorAll('.missions')
-    resultTitle.innerText= "Tasks: "+ missions.length;
-
-    clearButton.addEventListener('click',()=>{
-        lenth.pop(missions.length)
-        resultTitle.innerText= "Tasks: "+ lenth.length;
-        console.log(lenth)
-        document.getElementById('defaultDiv').style.display="block";
-        clearButton.style.display='none'
-        todoDiv.remove();
-        localStorage.clear();
-     })
-    })
-}
---
+++ script2.js
@@ -0,0 +1,222 @@
+        const action = document.getElementById('action');
+        const result = document.getElementById('result');
+        const button = document.getElementById('button');
+        const missionDescription = document.getElementById('missionDescription')
+        const hour = document.getElementById('day')
+        const minute = document.getElementById('hour')
+        const mNameError = document.getElementById('mNameError')
+        const mDescriptionError = document.getElementById('mDescriptionError')
+        const timeError = document.getElementById('timeError')
+        const resultTitle = document.getElementById('resultTitle')
+        const missionList = document.getElementById('missionList')
+        const submitButton = document.getElementById('submit')
+        const clearButton = document.getElementById('ClearButton')
+        
+
+        document.addEventListener('DOMContentLoaded', getItems());
+        button.addEventListener('click', ()=>{
+
+            if(action.value===''|| mDescriptionError.value===''||hour.value===''|| minute.value===''){
+                console.log("full form is not submited")
+                alert("Please, fill the complete form")
+            }
+            else{
+                // result.innerText =action.value;
+            addLocalStorage(action.value);
+            //remove default result div
+            document.getElementById('defaultDiv').style.display="none";
+
+                const paraDiv = document.createElement('div')
+                paraDiv.classList.add('missions')
+                const timeDiv = document.createElement('div');
+                const countDownSection = document.createElement('div');
+                countDownSection.classList.add('countDownSection')
+                const dataDiv = document.createElement('div');
+                dataDiv.classList.add('dataDiv')
+                const buttonDiv = document.createElement('div');
+                buttonDiv.classList.add('buttonDiv')
+                const para = document.createElement('li');
+                para.innerText = action.value;
+                const todoDes = document.createElement('p');
+                todoDes.innerHTML="Description: " + missionDescription.value;
+                const timeTitle = document.createElement('h3')
+                const hourCount = document.createElement('p')
+                const divider = document.createElement('p')
+                const minuteCount = document.createElement('p')
+                timeTitle.innerHTML ="Initiate at:"
+                hourCount.innerHTML = hour.value 
+                hourCount.classList.add('hourCount')
+                divider.innerText = '/'
+                minuteCount.innerHTML =  minute.value
+                minuteCount.classList.add('minuteCount')
+                
+                dataDiv.appendChild(para);
+                dataDiv.appendChild(todoDes);
+                //complete Button
+                const complete = document.createElement('button');
+                complete.innerHTML= '<i class="fa-solid fa-circle-check"></i>'
+                complete.classList.add('completeButton')
+                paraDiv.appendChild(complete);
+                const delButton = document.createElement('button');
+                //del Button
+                delButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+                delButton.classList.add('delButton')
+                delButton.classList.add('trashButton1')
+                paraDiv.appendChild(delButton);
+                result.appendChild(paraDiv);
+                buttonDiv.appendChild(complete)
+                buttonDiv.appendChild(delButton)
+                timeDiv.appendChild(timeTitle);
+                countDownSection.appendChild(hourCount);
+                countDownSection.appendChild(divider);
+                countDownSection.appendChild(minuteCount);
+                timeDiv.appendChild(countDownSection)
+
+                // adding to subMenu
+                paraDiv.appendChild(dataDiv)
+                paraDiv.appendChild(timeDiv)
+                paraDiv.appendChild(buttonDiv)
+
+                //resut input Fields
+                action.value ='';
+                missionDescription.value=''
+                minute.value=''
+                hour.value=''
+
+                //task counter
+                const missions = document.querySelectorAll('.missions')
+                resultTitle.innerText= "Tasks: "+ missions.length;
+                if(missions.length===0){
+                    clearButton.style.display="none"
+                    defaultDiv.style.display="block"
+                }
+            }
+        })
+        result.addEventListener('click',(e)=>{
+            const items= e.target;
+            const itemsParent1 = items.parentElement.parentElement;
+            const itemsParent = items.parentElement.parentElement.children[0].children[0].innerText;
+            if(items.classList[0] ==="delButton"){
+                itemsParent1.classList.add('delanimation');
+                delFunction(itemsParent);
+                itemsParent1.addEventListener('transitionend',()=>{
+                    itemsParent1.remove();
+                    //task Count
+                    const missions = document.querySelectorAll('.missions')
+                    console.log(missions.length)
+                    resultTitle.innerText= "Tasks: "+ (missions.length);
+                    if(missions.length===0){
+                        clearButton.style.display="none"
+                        defaultDiv.style.display="block"
+                    }
+                })
+                }
+                // Add single complete buttion Style
+                if(items.classList[0]==='completeButton'){
+                    itemsParent1.classList.add('completeMission')
+                }
+        })
+
+        //adding local storage
+        function addLocalStorage(data){
+            let names;
+            if(localStorage.getItem('names')=== null){
+                names =[];
+
+            }
+            else{
+                names = JSON.parse(localStorage.getItem('names'));
+            }
+            names.push(data);
+            localStorage.setItem('names', JSON.stringify(names))
+        }
+
+        // getItems Functions
+        function getItems(){
+            let names;
+            if(localStorage.getItem('names')=== null){
+                names =[];
+
+            }
+            else{
+                names = JSON.parse(localStorage.getItem('names'));
+            }
+            names.forEach(function(names){
+                document.getElementById('defaultDiv').style.display="none";
+                const paraDiv = document.createElement('div')
+                paraDiv.classList.add('missions')
+                const timeDiv = document.createElement('div');
+                const countDownSection = document.createElement('div');
+                countDownSection.classList.add('countDownSection')
+                const dataDiv = document.createElement('div');
+                dataDiv.classList.add('dataDiv')
+                const buttonDiv = document.createElement('div');
+                buttonDiv.classList.add('buttonDiv')
+                const para = document.createElement('li');
+                const delButton2 = document.createElement('button');
+                para.innerText = names;
+                delButton2.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+                delButton2.classList.add('delButton2')
+                delButton2.classList.add('trashButton1')
+                //complete Button
+                const complete = document.createElement('button');
+                complete.innerHTML= '<i class="fa-solid fa-circle-check"></i>'
+                complete.classList.add('completeButton')
+                //adding items on subDiv
+                dataDiv.appendChild(para);
+                buttonDiv.appendChild(complete);
+                buttonDiv.appendChild(delButton2);
+                //adding itm to main div
+                paraDiv.appendChild(dataDiv);
+                paraDiv.appendChild(buttonDiv);
+                result.appendChild(paraDiv);
+                //task Count
+                const missions = document.querySelectorAll('.missions')
+                console.log(missions.length)
+                resultTitle.innerText= "Tasks: "+ (missions.length);
+                if(missions.length===0){
+                    clearButton.style.display="none"
+                    defaultDiv.style.display="block"
+                }
+            })
+            result.addEventListener('click',(e)=>{
+            const items= e.target;
+            // const itemsParent1 = items.parentElement;
+            const itemsParent2 = items.parentElement.parentElement;
+            const itemsParent = items.parentElement.parentElement.children[0].children[0].innerText;
+            if(items.classList[0] ==="delButton2"){
+                    itemsParent2.classList.add('delanimation')
+                    delFunction(itemsParent);
+                    itemsParent2.addEventListener('transitionend',()=>{
+                    itemsParent2.remove();
+                    //task Count
+                    const missions = document.querySelectorAll('.missions')
+                    console.log(missions.length)
+                    resultTitle.innerText= "Tasks: "+ (missions.length);
+                    if(missions.length===0){
+                        clearButton.style.display="none"
+                        defaultDiv.style.display="block"
+                    }
+            })
+            }
+            // Add single complete buttion Style
+            if(items.classList[0]==='completeButton'){
+                itemsParent2.classList.add('completeMission')
+            }
+        })
+        }
+
+        //adding Del Function
+        function delFunction(itemsParent){
+            let names;
+            if(localStorage.getItem('names')=== null){
+                names =[];
+
+            }
+            else{
+                names = JSON.parse(localStorage.getItem('names'));
+            }
+            console.log(itemsParent)
+            names.splice(names.indexOf(itemsParent),1)
+            localStorage.setItem('names', JSON.stringify(names))
+        }
